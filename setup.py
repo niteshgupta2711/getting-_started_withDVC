b83d8c93374e77c6a86096641c6a00dd8449ec74
nitesh <guptanitesh2711@gmail.com>
2022-04-29 16:24:13 +0530

    some changes done to setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ from setuptools import setup,find_packages
 # every folder with "__init__" considered as package
 # packages=find_packages()
 setup(version='major.minor.patch',
-author='Nitesh',
+author='Nitesh'
+
 author_email='mail',
 long_description='long description',
- description='description',
- packages=['yourpackage'],
- name='lib_name',
- install_requires=['external packages','pandas','dvc']
- python_version='>=3.7'
- )
+description='description',
+packages=['yourpackage'],
+name='lib_name',
+install_requires=['external packages','pandas','dvc']
+python_version='>=3.7'
+)
  # pip install wheel
  # python setup.py sdist bdist_wheel
  # pip install ./pkg.wheel
---
+++ src/ingestion.py
@@ -0,0 +1,7 @@
+from utils.main import read_yaml
+import argparse
+def get_data(configPathForRemote):
+
+if __name__=='__main__':
+    args=argparse.ArgumentParser().add_argument('--config','-c',default='config/config.yaml')
+    print(args.config)
