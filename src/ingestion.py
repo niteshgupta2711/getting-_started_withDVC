9549007c74f4557e38d5f91c631adbd432a6ec6e
nitesh <guptanitesh2711@gmail.com>
2022-04-29 17:14:01 +0530

    first stage added

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,7 +1,30 @@
-from utils.main import read_yaml
+import os
+from src.utils.main import read_yaml
+from src.utils.main import make_dirs
+import pandas as pd
 import argparse
-def get_data(configPathForRemote):
+def get_data_tolocal(yamlpathconfig):
+    config_d=read_yaml(yamlpathconfig)
+    raw_source=config_d['data_source']
+    print(raw_source)
+    root_source=config_d['data']['data_source']
+    raw_dir=config_d['data']['raw_folder']
+    raw_directory=os.path.join('data',root_source)
+    print(raw_dir)
+    raw_folder_from_ics=os.path.join(raw_directory,raw_dir)
+    make_dirs([raw_folder_from_ics])
+    file_source=os.path.join(raw_folder_from_ics,'redwine.csv')
+    df=pd.read_csv(raw_source,delimiter=';')
+    df.to_csv(file_source,sep=',',index=False)
 
-if __name__=='__main__':
+    
+
+
+
+
+
+
+
+if (__name__)=='__main__':
     args=argparse.ArgumentParser().add_argument('--config','-c',default='config/config.yaml')
-    print(args.config)
+    get_data_tolocal(args.default)
--- src/utils/main.py
+++ src/utils/main.py
@@ -1,8 +1,12 @@
 import yaml,os
 def read_yaml(path: str) -> dict:
-    with open('path') as yamlfile:
+    with open(path) as yamlfile:
         content=yaml.safe_load(yamlfile)
     return content
+
+
+
+
 def make_dirs(join_path: list):
     for dir in join_path:
-        os.makedirs(dir)
+        os.makedirs(dir,exist_ok=True)
